86339a9f48588cef1b272fc69ea7fcbc7076502b
Matthew <mrk443@stern.nyu.edu>
2022-06-14 15:14:53 -0700

    adding header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps /*, AppContext */ } from "next/app";
+import Head from "next/head";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MK</title>
+        <meta property="og:title" content="MK" key="title" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
